0522fc4638643ac609a5acd40ca282dd96773542
Nicola Febbrari <n.febbrari@abletech.it>
2017-06-19 11:47:38 +0200

    new TaskInvoiceWidget plugin

--- TaskInvoiceWidget/TaskInvoiceWidgetDirective.js
+++ TaskInvoiceWidget/TaskInvoiceWidgetDirective.js
@@ -1,19 +1,7 @@
-function initMap(lat, lng) {
-	if (lat !== undefined && google !== undefined) {
-		var myLatLng = { lat: lat, lng: lng };
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 15,
-			center: myLatLng
-		});
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			map: map,
-		});
-	}
-}
+
 angular.module('arxivar.plugins.directives').directive('taskinvoicewidgetdirective', [
-	'TaskInvoiceWidget', 'pluginService', 'taskVariablesService', 'taskOperationsService', '_', 'moment', '$http',
-	function(TaskInvoiceWidget, pluginService, taskVariablesService, taskOperationsService, _, moment, $http) {
+	'TaskInvoiceWidget', 'pluginService', 'taskVariablesService', 'taskOperationsService', '_', 'moment',
+	function(TaskInvoiceWidget, pluginService, taskVariablesService, taskOperationsService, _, moment) {
 
 		return {
 			restrict: 'E',
@@ -22,6 +10,7 @@ angular.module('arxivar.plugins.directives').directive('taskinvoicewidgetdirecti
 			},
 			templateUrl: './Scripts/plugins/TaskInvoiceWidget/TaskInvoiceWidget.html',
 			link: function(scope, element, attrs, ctrls) {
+
 				const _settVariables = ({
 					booleanVariables,
 					stringVariables,
@@ -37,10 +26,24 @@ angular.module('arxivar.plugins.directives').directive('taskinvoicewidgetdirecti
 					scope.datafattura = moment(_.find(scope.variables, { name: 'Data fattura' }).value).format('L');
 					scope.datascadenza = moment(_.find(scope.variables, { name: 'Data scadenza' }).value).format('L');
 
-					var xmlhttp = new XMLHttpRequest();
-					var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + scope.indirizzo;
 
+					var xmlhttp = new XMLHttpRequest();
+
+					var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + scope.indirizzo;
 					xmlhttp.onreadystatechange = function() {
+						function initMap(lat, lng) {
+							if (lat !== undefined) {
+								var myLatLng = { lat: lat, lng: lng };
+								var map = new google.maps.Map(document.getElementById('map'), {
+									zoom: 15,
+									center: myLatLng
+								});
+								var marker = new google.maps.Marker({
+									position: myLatLng,
+									map: map,
+								});
+							}
+						}
 						if (this.readyState === 4 && this.status === 200) {
 							var response = JSON.parse(this.responseText);
 							initMap(response.results[0].geometry.location.lat, response.results[0].geometry.location.lng);
@@ -48,31 +51,27 @@ angular.module('arxivar.plugins.directives').directive('taskinvoicewidgetdirecti
 					};
 					xmlhttp.open('GET', url, true);
 					xmlhttp.send();
-
-					// $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + scope.indirizzo)
-					// 	.then(function (response) {
-
-
-					// 	});
 				};
-				var $mainContainer = $(element).find('div.arx-' + testTask.plugin.name.toLowerCase());
-				if ($mainContainer.length > 0) {
-					$mainContainer.addClass(scope.instanceId);
-				}
-				if (!_.isNil(scope.taskDto.id)) {
-					if (_.isNil(this.operationVariables)) {
-						taskOperationsService.getTaskOperations(scope.taskDto.id)
-							.then((operations) => {
-								scope.operationVariables = operations.taskWorkVariablesOperation;
-								_settVariables(operations.taskWorkVariablesOperation.processVariablesFields);
-
-							});
+				$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyC341o4aabs3DDquZfq6BCUihpmD6pgPKo', function() {
+					var $mainContainer = $(element).find('div.arx-' + TaskInvoiceWidget.plugin.name.toLowerCase());
+					if ($mainContainer.length > 0) {
+						$mainContainer.addClass(scope.instanceId);
 					}
-					scope.variablesModel = {};
-				} else {
-					scope.variables = [];
-					scope.variablesModel = {};
-				}
+					if (!_.isNil(scope.taskDto.id)) {
+						if (_.isNil(this.operationVariables)) {
+							taskOperationsService.getTaskOperations(scope.taskDto.id)
+								.then((operations) => {
+									scope.operationVariables = operations.taskWorkVariablesOperation;
+									_settVariables(operations.taskWorkVariablesOperation.processVariablesFields);
+
+								});
+						}
+						scope.variablesModel = {};
+					} else {
+						scope.variables = [];
+						scope.variablesModel = {};
+					}
+				});
 
 			}
 		};
